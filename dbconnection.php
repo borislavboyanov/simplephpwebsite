63177f90120088fa588dd89a1699b892dd69b8b2
borislavboyanov <50285198+borislavboyanov@users.noreply.github.com>
2019-08-27 03:42:20 +0300

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -2,7 +2,7 @@
   $dbServername = "localhost";
   $dbUsername = "root";
   $dbPassword = "";
-  $dbName = "diplomnarabota";
+  $dbName = "simplephpwebsite";
 
   // Create connection
   $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
